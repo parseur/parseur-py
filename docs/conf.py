b0cc7df9908c169b1a4fac911d82c8d53c98ee41
Laurent Pauloin <laurent@parseur.com>
2025-12-08 12:03:43 +0700

    Bump to version 1.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "parseur-py"
 copyright = "2025, Parseur Team"
 author = "Parseur Team"
-release = "1.4.1"
+release = "1.5.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
